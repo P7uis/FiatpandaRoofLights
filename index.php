d148816b69b6f01986a9839f91421ce261595c47
Wesley <P7uisdev@gmail.com>
2019-01-03 10:46:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
- if($_GET['btn5']){echo shell_exec("echo '5' > /home/pi/scripts/numbers.txt");;}
- if($_GET['btn4']){echo shell_exec("echo '4' > /home/pi/scripts/numbers.txt");;}
+ if($_GET['btn5']){echo shell_exec("echo '5' > /home/pi/scripts/number.txt");;}
+ if($_GET['btn4']){echo shell_exec("echo '4' > /home/pi/scripts/number.txt");;}
 ?>
 
 <!DOCTYPE html>
